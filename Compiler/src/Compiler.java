5a4b9d92ab12319d79c7ce1fee0237ba3569cc8f
Enrique Gambra <gambra7@gmail.com>
2020-02-10 17:03:01 -0500

    Added in bufferreader and file needed to read in the files.

--- Compiler/src/Compiler.java
+++ Compiler/src/Compiler.java
@@ -1,20 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.io.*;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
-/**
- *
- * @author Owner
- */
+
 public class Compiler {
+    
+    ArrayList<String> linesIn = new ArrayList<>();
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        readInFile();
+    }
+    
+    public static void readInFile(){
+        //Get the file path
+        String filePath = JOptionPane.showInputDialog("Give the file path for the text file you'd like to write in.");
+        
+        //Using a sample file path for now... need double \\ to avoid the escape character in a string
+        File pascalFile = new File("C:\\Users\\Owner\\github-Repos\\Pascal-Compiler\\sample_pascal_code.txt");
+        
+        try {
+            //Trying to now open the file
+            BufferedReader br = new BufferedReader(new FileReader(pascalFile));
+            String linesRead;
+            //Reading every line
+            while((linesRead = br.readLine()) != null){
+                System.out.println(linesRead);
+            }
+        } 
+        catch (Exception ex) {
+            System.out.println("File not found!");
+        }
+        
     }
     
 }
