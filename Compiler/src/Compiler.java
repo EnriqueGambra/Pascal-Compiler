6161fafc9b290057d4dd5fc36b5c2112da1164a3
Enrique Gambra <gambra7@gmail.com>
2020-02-13 00:22:24 -0500

    Finished the method 'parseWord' so that it now parses the strings to individual characters splitting the operators and tokwords and toknumbers.

--- Compiler/src/Compiler.java
+++ Compiler/src/Compiler.java
@@ -8,28 +8,66 @@ import javax.swing.JOptionPane;
 
 
 public class Compiler {
-    
-    static ArrayList<String> words = new ArrayList<>();
 
+    //Creating a new arraylist that has words with operators seperated
+    static ArrayList<String> wordsWithOp = new ArrayList<>();
+    
     public static void main(String[] args) {
-        wordsToChar();
-        readInFile();
-        for(int i = 0; i < words.size(); i++){
-            System.out.println(words.get(i));
+        readInFile();    
+        
+        for(int i = 0; i < wordsWithOp.size(); i++){
+            System.out.println(wordsWithOp.get(i));
         }
+
+        
     }
     
-    public static void wordsToChar(){
-        //This method is going to go through the words array and split up
-        //the words that have operators included within them.
-        String value = "#`~!#$%^，";
-        String pattern = "(?U)[\\p{Punct}&&[^@',&]]";
-        Pattern r = Pattern.compile(pattern);    // Create a Pattern object
-        Matcher m = r.matcher(value);            // Now create matcher object.
-        while (m.find()) {
-            System.out.println("Found value: " + m.group());
+    public static void parseWord(String value){
+        //This method is going to go through the words array and split up the words that have operators included within them and then store
+        //them in the array wordsWithOp
+        
+        //This pattern is looking for special character indicating an operator
+        String pattern = "[^a-zA-Z0-9]";
+        Pattern r = Pattern.compile(pattern);    
+        Matcher m = r.matcher(value);            
+        
+        //variable that indicates to true or false depending on if there is a special character
+        boolean containsOperator = false;
+        
+        //This while loop is checking to see if any special characters are within
+        while(m.find()){
+            //Since there is a special character, set to containsOperator to true
+            containsOperator = true;
+            //Get the index of the special character
+            int index = value.indexOf(m.group());
+            //If the value is at index 0, indicating it is before any tokword at the front of a string
+            if(index == 0){
+                //Add the special character to the arraylist
+                wordsWithOp.add(Character.toString(value.charAt(index)));
+                //Shorten the string so it doesn't include the special character just added to the array
+                value = value.substring(index+1, value.length());
+            }
+            //Not at the front of the array, we now have to create a substring of the new string to now 
+            //get the proper special characters at the end of the array
+            else{
+                //Getting the location of the actual tokword
+                String tempWord = value.substring(0, index);
+                //Add it to the array
+                wordsWithOp.add(tempWord);
+                //Add the special character to the array
+                wordsWithOp.add(Character.toString(value.charAt(index)));
+                //Shorten the string again to not include the special character just taken out
+                value = value.substring(index + 1, value.length());
+            }
         }
+        
+        //If it doesn't contain an operator... add the value to the arraylist
+        if(!containsOperator){
+            wordsWithOp.add(value);
+        }
+            
     }
+    
     
     public static void getToken(){
         //This method will iterate through the words array and determine which
@@ -54,9 +92,15 @@ public class Compiler {
             while((linesRead = br.readLine()) != null){
                 //Split the string in with every space, creating and seperating every 'word' in the line
                 String[] tempWords = linesRead.split(" "); 
-                //Go through each 'word' and store it into an arraylist and keep them stored in the arraylist
+                //Go through each 'word' and send it to the wordToChar() method
+                //to be further analyzed
                 for(int i = 0; i < tempWords.length; i++){
-                    words.add(tempWords[i]);
+                    //Gets rid of any white space
+                    tempWords[i] = tempWords[i].trim();
+                    //Checks to see if the line is empty... if it is, skip
+                    if(!tempWords[i].isEmpty()){
+                        parseWord(tempWords[i]);
+                    }
                 }
             }
         } 
