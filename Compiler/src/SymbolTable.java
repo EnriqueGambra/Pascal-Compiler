079d1bb44eed6f04ae262a1c17a5a26234885d71
Enrique Gambra <gambra7@gmail.com>
2020-03-02 22:03:34 -0500

    Fixed the compiler to correctly identify identifiers in all cases. Added in more symbols to the symbol table.

--- Compiler/src/SymbolTable.java
+++ Compiler/src/SymbolTable.java
@@ -21,6 +21,11 @@ public class SymbolTable {
         sTable.put("BEGIN", "tokbegin");
         sTable.put("END", "tokend");
         sTable.put(".", "tokperiod");
+        sTable.put("{", "tokbegincomment");
+        sTable.put("}", "tokendcomment");
+        sTable.put(",", "tokcomma");
+        sTable.put("(", "tokopenparen");
+        sTable.put(")", "tokcloseparen");
     }
     
     public void getSTableToken(){
@@ -30,7 +35,6 @@ public class SymbolTable {
         boolean skipCurrent = false;
         
         for(int i = 0; i < tokenArr.length; i++){
-            
             if(skipCurrent){
                 skipCurrent = false;
                 continue;
@@ -40,17 +44,17 @@ public class SymbolTable {
             String token = tokenArr[i][1];
             
             if(sTable.containsKey(word.toUpperCase())){
-                //Checking to correctly identify identifiers after an equals sign was detected
-                if(word.equals("=")){
-                    if(tokenArr[i-2][1].equals("tokword")){
-                        sTableArr[i-1][1] = "tokidentifier";
-                    }
-                }
-                //If the word equals program, the next word must be identifier and skip the loop
-                else if(word.equals("PROGRAM")){
+                if(word.equals("PROGRAM")){
                     sTableArr[i+1][0] = tokenArr[i+1][0];
                     sTableArr[i+1][1] = "tokidentifier";
                     skipCurrent = true;
+                }
+                //If the word is a semi colon, equals, comma, or close parentheses check to see if the value prior is not a number
+                else if(word.equals(";") || word.equals("=") || word.equals(",") || word.equals(")"))
+                {
+                    if(sTableArr[i-1][1].equals("tokword")){
+                        sTableArr[i-1][1] = "tokidentifier";
+                    }
                 }
                 
                 sTableArr[i][0] = word;
